aa3eee0e51cf5b093d34cbf7fd7edbcfd4e6319e
coranos <>
2019-09-25 21:38:34 -0500

    adding split key vanity

--- app/scripts/vanity-util.js
+++ app/scripts/vanity-util.js
@@ -1,14 +1,25 @@
-const nacl = require( '../../libraries/tweetnacl/nacl.js' );
+const EC = require('elliptic').ec;
+const ec = new EC('curve25519');
 
 const bananoUtil = require( './banano-util.js' );
 
-// const getRandomHex32 = () => {
-//   return crypto.randomBytes(32).toString('hex');
-// };
+const addPublicKeys = (a0, b0) => {
+  const aKey = ec.keyFromPublic(a0, 'hex');
+  const bKey = ec.keyFromPublic(b0, 'hex');
+  const a1= aKey.getPublic(true, 'hex').toUpperCase();
+  const b1= bKey.getPublic(true, 'hex').toUpperCase();
+  console.log('addPublicKeys', 'a', a0, a1, aKey.pub);
+  console.log('addPublicKeys', 'b', b0, b1, bKey.pub);
+  // const cKey = aKey.pub.add(bKey.pub);
+  // const c1= cKey.getPublic(true, 'hex');
+  // console.log('addPublicKeys', 'c', c0, c1);
+  return a0;
+};
 
-const isSplitKeyVanity = (privateKeyBytes, vanityRegex) => {
+const isSplitKeyVanity = (clientPublicKey, privateKeyBytes, vanityRegex) => {
   const privateKey = bananoUtil.bytesToHex(privateKeyBytes);
-  const publicKey = bananoUtil.getPublicKey(privateKey);
+  const rawPublicKey = bananoUtil.getPublicKey(privateKey);
+  const publicKey = addPublicKeys(rawPublicKey, clientPublicKey);
   const account = bananoUtil.getAccount(publicKey);
   const isSplitKeyVanityFlag = vanityRegex.test(account);
   // console.log('isSplitKeyVanity',
@@ -19,13 +30,13 @@ const isSplitKeyVanity = (privateKeyBytes, vanityRegex) => {
   return isSplitKeyVanityFlag;
 };
 
-const generateSplitKeyVanity = (secretPublicKey, vanityRegexStr) => {
+const generateSplitKeyVanity = (clientPublicKey, vanityRegexStr) => {
   const vanityRegex = new RegExp(vanityRegexStr);
   const privateKeyBytes = new Uint8Array(32);
-  let isSplitKeyVanityFlag = isSplitKeyVanity(privateKeyBytes, vanityRegex);
+  let isSplitKeyVanityFlag = isSplitKeyVanity(clientPublicKey, privateKeyBytes, vanityRegex);
   while (!isSplitKeyVanityFlag) {
     bananoUtil.incrementBytes(privateKeyBytes);
-    isSplitKeyVanityFlag = isSplitKeyVanity(privateKeyBytes, vanityRegex);
+    isSplitKeyVanityFlag = isSplitKeyVanity(clientPublicKey, privateKeyBytes, vanityRegex);
   }
   const privateKey = bananoUtil.bytesToHex(privateKeyBytes);
   const publicKey = bananoUtil.getPublicKey(privateKey);
--- test/unit/vanity-test.js
+++ test/unit/vanity-test.js
@@ -12,10 +12,10 @@ const bananojs = testUtil.getBananojsWithMockApi();
 
 describe('vanity', () => {
   it('generateSplitKeyVanity account matches expected', () => {
-    const secretPrivateKey = bananoTest.privateKey;
-    const secretPublicKey = bananojs.getPublicKey(secretPrivateKey);
+    const clientPrivateKey = bananoTest.privateKey;
+    const clientPublicKey = bananojs.getPublicKey(clientPrivateKey);
     const vanityRegex = 'ban_1c.*?';
-    const vanityInfo = bananojs.generateSplitKeyVanity(secretPublicKey, vanityRegex);
+    const vanityInfo = bananojs.generateSplitKeyVanity(clientPublicKey, vanityRegex);
     expect(vanityInfo).to.deep.equal({
       account: 'ban_1cr4mm7po9h9x3c6f5sxzfuartbpq9aqo7zobjp6p8yob4gawen6junsx8x1',
       privateKey: '00000000000000000000000000000000000000000000000000000000000000FF',

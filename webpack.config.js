d32847500bfb97f74c641f81005b7c89e93dbaef
coranos <>
2020-02-28 20:26:06 -0600

    debugging webpack

--- webpack.config.js
+++ webpack.config.js
@@ -4,11 +4,16 @@ const nodeExternals = require('webpack-node-externals');
 
 module.exports = {
   target: 'node',
-  mode: 'production',
+  mode: 'development',
   entry: './index.js',
   output: {
     filename: 'bananocoin-bananojs.js',
     path: path.resolve(__dirname, 'dist'),
   },
-  externals: [nodeExternals()]
+  externals: [nodeExternals({
+    whitelist: [
+      'blakejs',
+      '../../libraries/tweetnacl/nacl.js',
+    ],
+  })],
 };

08b4f2b353fa548d013211c7797690e59ebf584b
coranos <coranos@users.noreply.github.com>
2021-01-01 20:49:00 -0600

    moving ledger hw wallet code to ledger-hw project

--- docs/banano-cli.md
+++ docs/banano-cli.md
@@ -17,7 +17,7 @@
         banano getaccount publicKey ACA68A2D52FE17BAB36D48456569FE7F91F23CB57B971B13FAF236EBBCC7FA94
         banano getaccount account ban_3d78japo7ziqqcsptk47eonzwzwjyaydcywq5ebzowjpxgyehynnjc9pd5zj
 
-# check pending using private key:
+# check pending using public key:
 
         npm start bcheckpending ${account} ${maxAccountsPending}
 
@@ -48,99 +48,16 @@
 
 # convert amount to raw:
 
-        npm start bamountraw 100000000000000000000000000000
+        npm start bamountraw 1
+
+        bananojs
+        bamountraw response 100000000000000000000000000000
 
 # send using private key:
 
         npm start bsendraw ${privateKey} ${destAccount} ${amountRaw}
 
         npm start bsendraw 1111111111111111111111111111111111111111111111111111111111111111 ban_1coranoshiqdentfbwkfo7fxzgg1jhz6m33pt9aa8497xxfageuskroocdxa 100000000000000000000000000000
-
-        bananojs
-        banano sendbanano response BF3BA5C6F91D52E88658E6AB800237C4023AD59392B4AB203EBA1E5BF706E535
-
-# check account info
-
-        npm start baccountinfo ban_1coranoshiqdentfbwkfo7fxzgg1jhz6m33pt9aa8497xxfageuskroocdxa
-
-        bananojs
-        banano accountinfo response {
-          frontier: '41E7FF66C785F3DE2F192BA05C8DEBDC2B33D89B85C5BB49B4F219C8112A5BC8',
-          open_block: 'B6BD40F6F400BF0D81F2A28218039EBB1E6B9EE2764A0EFF344F7B9A123D5067',
-          representative_block: '41E7FF66C785F3DE2F192BA05C8DEBDC2B33D89B85C5BB49B4F219C8112A5BC8',
-          balance: '100000000000000000000000000000000000',
-          modified_timestamp: '1604070386',
-          block_count: '704',
-          account_version: '0',
-          confirmation_height: '704',
-          representative: 'ban_1fomoz167m7o38gw4rzt7hz67oq6itejpt4yocrfywujbpatd711cjew8gjj',
-          balanceParts: {
-            majorName: 'banano',
-            minorName: 'banoshi',
-            banano: '1000000',
-            banoshi: '0',
-            raw: '0'
-          },
-          balanceDescription: '1,000,000 banano',
-          balanceDecimal: '1000000.0000000000000000000000000000000'
-        }
-
-### with ledger
-
-# get the account for the private key:
-
-        npm start blgetaccount ${accountIx}
-
-        npm start blgetaccount 0
-
-        bananojs
-        banano getaccount publicKey ACA68A2D52FE17BAB36D48456569FE7F91F23CB57B971B13FAF236EBBCC7FA94
-        banano getaccount account ban_3d78japo7ziqqcsptk47eonzwzwjyaydcywq5ebzowjpxgyehynnjc9pd5zj
-
-# check pending using private key:
-
-        npm start bcheckpending ${accountIx} ${maxAccountsPending}
-
-        npm start blcheckpending 0 10
-
-        bananojs
-        banano checkpending response {
-          blocks: {
-            ban_3d78japo7ziqqcsptk47eonzwzwjyaydcywq5ebzowjpxgyehynnjc9pd5zj: {
-              '48818DC7E09AA8EE12A62D23FBB4AD0D687087C8B3D2C5B5835951162D5DA615': '100000000000000000000000000000'
-            }
-          }
-        }
-
-# recieve pending using private key:
-
-        npm start blreceive ${accountIx} ${hash}
-
-        npm start blreceive 0 E6135B09A3DC34455D65E14B675ACE4DB87ED5B841FBCFA4234B09097F8ADD69
-
-        bananojs
-        banano receive response {
-          pendingCount: 1,
-          pendingBlocks: [
-            'E6135B09A3DC34455D65E14B675ACE4DB87ED5B841FBCFA4234B09097F8ADD69'
-          ],
-          receiveCount: 1,
-          receiveBlocks: [
-            '6FAE6888AE03C6109A22BF973FB9C0D9684610835B50474712C4BC86A9FB636C'
-          ],
-          pendingMessage: 'pending 1 blocks, of max 10.',
-          receiveMessage: 'received 1 blocks.'
-        }
-
-# convert amount to raw:
-
-        npm start bamountraw 100000000000000000000000000000
-
-# send using private key:
-
-        npm start blsendraw ${accountIx} ${destAccount} ${amountRaw}
-
-        npm start blsendraw 0 ban_1coranoshiqdentfbwkfo7fxzgg1jhz6m33pt9aa8497xxfageuskroocdxa 100000000000000000000000000000
 
         bananojs
         banano sendbanano response BF3BA5C6F91D52E88658E6AB800237C4023AD59392B4AB203EBA1E5BF706E535
--- main.js
+++ main.js
@@ -7,99 +7,14 @@ const camoUtil = require('./app/scripts/camo-util.js');
 const loggingUtil = require('./app/scripts/logging-util.js');
 const depositUtil = require('./app/scripts/deposit-util.js');
 const crypto = require('crypto');
-const BananoHwApp = require('hw-app-nano').Banano;
-const transportNodeHid = require('@ledgerhq/hw-transport-node-hid');
 
 const configs = {};
 configs.banano = {};
-configs.banano.walletPrefix = `44'/198'/`;
 configs.banano.prefix = index.BANANO_PREFIX;
 configs.banano.bananodeUrl = 'https://kaliumapi.appditto.com/api';
 configs.nano = {};
 configs.nano.prefix = index.NANO_PREFIX;
 configs.nano.bananodeUrl = 'https://app.natrium.io/api';
-
-const getLedgerPath = (config, accountIndex) => {
-  return `${config.walletPrefix}${accountIndex}'`;
-};
-
-const getLedgerAccountData = async (config, index) => {
-  // https://github.com/BananoCoin/bananovault/blob/master/src/app/services/ledger.service.ts#L128
-  try {
-    const paths = await transportNodeHid.default.list();
-    const path = paths[0];
-    const transport = await transportNodeHid.default.open(path);
-    const banHwAppInst = new BananoHwApp(transport);
-    const accountData = await banHwAppInst.getAddress(getLedgerPath(config, index));
-    accountData.account = accountData.address;
-    delete accountData.address;
-    return accountData;
-  } catch (error) {
-    console.trace('banano getaccount error', error.message);
-  }
-};
-
-const getLedgerAccountSigner = async (config, accountIx) => {
-  /* istanbul ignore if */
-  if (config === undefined) {
-    throw Error('config is a required parameter.');
-  }
-  /* istanbul ignore if */
-  if (accountIx === undefined) {
-    throw Error('accountIx is a required parameter.');
-  }
-  // https://github.com/BananoCoin/bananovault/blob/master/src/app/services/ledger.service.ts#L379
-  const paths = await transportNodeHid.default.list();
-  const path = paths[0];
-  const transport = await transportNodeHid.default.open(path);
-  const banHwAppInst = new BananoHwApp(transport);
-  const signer = {};
-  const ledgerPath = getLedgerPath(config, accountIx);
-  const accountData = await banHwAppInst.getAddress(ledgerPath);
-  signer.getPublicKey = () => {
-    return accountData.publicKey;
-  };
-  signer.getAccount = () => {
-    return accountData.address;
-  };
-  signer.signBlock = async (blockData) => {
-    // console.log('signer.signBlock', 'blockData', blockData);
-    const hwBlockData = {};
-    if (blockData.previous == '0000000000000000000000000000000000000000000000000000000000000000') {
-      hwBlockData.representative = blockData.representative;
-      hwBlockData.balance = blockData.balance;
-      hwBlockData.sourceBlock = blockData.link;
-    } else {
-      hwBlockData.previousBlock = blockData.previous;
-      hwBlockData.representative = blockData.representative;
-      hwBlockData.balance = blockData.balance;
-      hwBlockData.recipient = index.getBananoAccount(blockData.link);
-
-      const cacheBlockData = {};
-      const cacheBlocks = await bananodeApi.getBlocks([blockData.previous], true);
-      // console.log('signer.signBlock', 'cacheBlocks', cacheBlocks);
-      const cacheBlock = cacheBlocks.blocks[blockData.previous];
-      // console.log('signer.signBlock', 'cacheBlock', cacheBlock);
-      cacheBlockData.previousBlock = cacheBlock.previous;
-      cacheBlockData.representative = cacheBlock.representative;
-      cacheBlockData.balance = cacheBlock.balance;
-      cacheBlockData.recipient = index.getBananoAccount(cacheBlock.link);
-      // console.log('signer.signBlock', 'cacheBlockData', cacheBlockData);
-      try {
-        // const cacheResponse =
-        await banHwAppInst.cacheBlock(ledgerPath, cacheBlockData, cacheBlock.signature);
-        // console.log('signer.signBlock', 'cacheResponse', cacheResponse);
-      } catch (error) {
-        console.log('signer.signBlock', 'error', error.message);
-        console.trace(error);
-      }
-    }
-
-    // console.log('signer.signBlock', 'hwBlockData', hwBlockData);
-    return await banHwAppInst.signBlock(ledgerPath, hwBlockData);
-  };
-  return signer;
-};
 
 const commands = {};
 
@@ -285,58 +200,6 @@ commands['getseed'] = async () => {
   console.log('getseed response', response);
 };
 
-commands['blgetaccount'] = async (index) => {
-  const config = configs.banano;
-  bananodeApi.setUrl(config.bananodeUrl);
-  const accountData = await getLedgerAccountData(config, index);
-  console.log('banano getaccount publicKey', accountData.publicKey);
-  console.log('banano getaccount account', accountData.account);
-};
-
-commands['blcheckpending'] = async (index, count) => {
-  if (index == undefined) {
-    throw Error('index is a required parameter');
-  }
-  if (count == undefined) {
-    throw Error('count is a required parameter');
-  }
-  const config = configs.banano;
-  bananodeApi.setUrl(config.bananodeUrl);
-  const accountData = await getLedgerAccountData(config, index);
-  const account = accountData.account;
-  console.log('banano checkpending accountData', account);
-  const pending = await bananodeApi.getAccountsPending([account], parseInt(count));
-  console.log('banano checkpending response', pending);
-};
-
-commands['blreceive'] = async (index, specificPendingBlockHash) => {
-  const config = configs.banano;
-  bananodeApi.setUrl(config.bananodeUrl);
-  const accountSigner = await getLedgerAccountSigner(config, index);
-  const account = accountSigner.getAccount();
-  let representative = await bananodeApi.getAccountRepresentative(account);
-  if (!(representative)) {
-    representative = account;
-  }
-  try {
-    const response = await depositUtil.receive(loggingUtil, bananodeApi, account, accountSigner, representative, specificPendingBlockHash, config.prefix);
-    console.log('banano receive response', response);
-  } catch (error) {
-    console.trace( error);
-  }
-};
-
-commands['blsendraw'] = async (index, destAccount, amountRaw) => {
-  const config = configs.banano;
-  bananodeApi.setUrl(config.bananodeUrl);
-  const accountSigner = await getLedgerAccountSigner(config, index);
-  try {
-    const response = await bananoUtil.sendFromPrivateKey(bananodeApi, accountSigner, destAccount, amountRaw, config.prefix);
-    console.log('banano sendbanano response', response);
-  } catch (error) {
-    console.log('banano sendbanano error', error.message);
-  }
-};
 
 const run = async () => {
   console.log('bananojs');

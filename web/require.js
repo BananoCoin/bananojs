2675fc78186a1c492f99256382d5c668e2d670b9
coranos <coranos@users.noreply.github.com>
2021-03-12 19:07:58 -0600

    2.4.3 forgot to update browser version to https as well.

--- web/require.js
+++ web/require.js
@@ -52,6 +52,72 @@ if (!window.bananocoin) {
 if (!window.bananocoin.bananojs) {
   window.bananocoin.bananojs = {};
 }
+window.bananocoin.bananojs.http = {};
+window.bananocoin.bananojs.http.request = (requestOptions, requestWriterCallback) => {
+  const LOG_HTTP = false;
+  const xmlhttp = new XMLHttpRequest();
+  const url = 'https://' + requestOptions.hostname + requestOptions.path;
+  xmlhttp.open(requestOptions.method, url, true);
+  Object.keys(requestOptions.headers).forEach((headerName) => {
+    if (headerName == 'Content-Length') {
+      // skip unsafe header warning
+    } else {
+      const headerValue = requestOptions.headers[headerName];
+      xmlhttp.setRequestHeader(headerName, headerValue);
+    }
+  });
+  xmlhttp.timeout = requestOptions.timeout;
+
+  const requestWriter = {};
+  requestWriter.listeners = {};
+  requestWriter.write = (body) => {
+    if (LOG_HTTP) {
+      console.log('https send', body);
+    }
+    xmlhttp.send(body);
+  };
+  requestWriter.end = () => {
+  };
+  requestWriter.listeners['end'] = () => {
+  };
+  requestWriter.listeners['data'] = () => {
+  };
+  requestWriter.listeners['error'] = () => {
+  };
+  requestWriter.on = (key, fn) => {
+    requestWriter.listeners[key] = fn;
+  };
+
+  requestWriterCallback(requestWriter);
+
+  const end = () => {
+    const endFn = requestWriter.listeners['end'];
+    endFn();
+  };
+
+  xmlhttp.onreadystatechange = function() {
+    if (this.readyState == 4) {
+      if (LOG_HTTP) {
+        console.log('https end', this.responseText);
+      }
+      if (this.status == 200) {
+        const fn = requestWriter.listeners['data'];
+        fn(this.responseText);
+        end();
+      } else {
+        const fn = requestWriter.listeners['error'];
+        const error = {};
+        error.responseText = this.responseText;
+        error.readyState = this.readyState;
+        error.status = this.status;
+        fn(error);
+        end();
+      }
+    }
+  };
+
+  return requestWriter;
+};
 window.bananocoin.bananojs.https = {};
 window.bananocoin.bananojs.https.request = (requestOptions, requestWriterCallback) => {
   const LOG_HTTP = false;

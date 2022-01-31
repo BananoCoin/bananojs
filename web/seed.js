16b95df4b0b68d066f51e5acce10c2cfdaae2451
coranos <coranos@users.noreply.github.com>
2022-01-30 21:04:39 -0600

    added seed decryption comment

--- web/seed.js
+++ web/seed.js
@@ -2,3 +2,9 @@ const loadSeeds = () => {
   document.getElementById('seed1').value = '0000000000000000000000000000000000000000000000000000000000000000';
   document.getElementById('seed2').value = 'FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF';
 };
+
+/* to encrypt and decrypt seeds use the below code
+CryptoJS.AES.encrypt(this.wallet.seed, this.wallet.password).toString();
+const decryptedBytes = CryptoJS.AES.decrypt(this.wallet.seed, password);
+const decryptedSeed = decryptedBytes.toString(CryptoJS.enc.Utf8);
+*/
